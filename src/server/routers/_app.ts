75cef7347a10f0f88a51164a9b037fb8b5ec5786
Jonathan Kowa <jonathan@kowa.io>
2025-04-02 22:07:43 +0200

    init python project

--- src/server/routers/_app.ts
+++
@@ -1,8 +0,0 @@
-import { router } from '../trpc';
-import { excelRouter } from './excel';
-
-export const appRouter = router({
-  excel: excelRouter,
-});
-
-export type AppRouter = typeof appRouter; 
